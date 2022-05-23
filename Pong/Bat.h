07bec1d4f6976a1a5e5224892688f74de0e9f79b
Rayven <rayven.burns@gmail.com>
2022-05-23 18:35:22 -0400

    updated gitignore and added comments

--- Pong/Bat.h
+++ Pong/Bat.h
@@ -1,4 +1,4 @@
-#pragma once   
+#pragma once   //prevents the file from being processed by the compiler more than once
 #include <SFML/Graphics.hpp>
 
 using namespace sf;
@@ -6,9 +6,9 @@ using namespace sf;
 class Bat {
 
 private:
-	Vector2f m_Position;
+	Vector2f m_Position; //hold the horiz. and vert. position of the player's bat
 
-	//A rectangle shape object
+	//A rectangle shape object - actual bat that appears on the screen
 	RectangleShape m_Shape;
 
 	float m_Speed = 1000.0f;
@@ -16,6 +16,7 @@ private:
 	bool m_MovingLeft = false;
 
 public:
+	//constructor (they have no return type, not even void)
 	Bat (float startX, float startY);
 
 	FloatRect getPosition();
@@ -30,6 +31,6 @@ public:
 
 	void stopRight();
 
-	void update(Time dt);
+	void update(Time dt); //will be used to calc. how to move the bat each frame
 
 };
