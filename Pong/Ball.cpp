0d5031b9960641cee680f2b4b13a256eeab81a78
Rayven <rayven.burns@gmail.com>
2022-05-26 13:05:55 -0400

    added getter functions

--- Pong/Ball.cpp
+++ Pong/Ball.cpp
@@ -9,3 +9,18 @@ Ball::Ball (float startX, float startY) {
 	m_Shape.setSize(sf::Vector2f(10, 10));
 	m_Shape.setPosition(m_Position);
 }
+
+//Getter function
+FloatRect Ball::getPosition() {
+	return m_Shape.getGlobalBounds();
+}
+
+//Getter function
+RectangleShape Ball::getShape() {
+	return m_Shape;
+}
+
+//Getter function
+float Ball::getXVelocity() {
+	return m_DirectionX;
+}
